98c707d0dd6a58ed5cd84e29f91222172185321a
Dmytro <saindi777@gmail.com>
2023-07-14 14:32:49 +0300

    use classes to describe tables

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,18 @@
 from datetime import datetime
-from sqlalchemy import MetaData, Table, Boolean, Column, Integer, String, TIMESTAMP
 
-metadata = MetaData()
+from sqlalchemy import Boolean, Column, Integer, String, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
 
-user = Table(
-    "user",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("email", String, nullable=False),
-    Column("hashed_password", String, nullable=False),
-    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("is_active", Boolean, default=True, nullable=False),
-    Column("is_superuser", Boolean, default=False, nullable=False),
-    Column("is_verified", Boolean, default=False, nullable=False),
-)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    is_verified = Column(Boolean, default=False, nullable=False)
