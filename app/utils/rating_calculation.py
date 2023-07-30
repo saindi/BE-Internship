a0be4076ed013627ad761db6fc33b3a4c81c8b4e
Dmytro <saindi777@gmail.com>
2023-07-30 14:28:38 +0300

    refactoring rating calc

--- app/utils/rating_calculation.py
+++ app/utils/rating_calculation.py
@@ -1,8 +1,6 @@
 
 def get_rating(results) -> float:
-    sum_correct, count_questions = 0, 0
-    for result in results:
-        sum_correct += result.count_correct_answers
-        count_questions += result.count_questions
+    sum_correct = sum(result.count_correct_answers for result in results)
+    count_questions = sum(result.count_questions for result in results)
 
     return sum_correct / count_questions
