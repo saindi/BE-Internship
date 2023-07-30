38a4cdae0df8c7deaee5686df375e85839b36104
Dmytro <saindi777@gmail.com>
2023-07-30 19:55:58 +0300

    optimization csv

--- app/utils/generate_csv.py
+++ app/utils/generate_csv.py
@@ -14,7 +14,7 @@ def get_csv(flattened_data):
     return io.BytesIO(output.getvalue().encode())
 
 
-def generate_csv_data_as_result(data: dict):
+def flatten_data(data):
     flattened_data = []
     for question in data["questions"]:
         row = {
@@ -28,24 +28,16 @@ def generate_csv_data_as_result(data: dict):
             "user_answers": ", ".join(question["user_answers"])
         }
         flattened_data.append(row)
+    return flattened_data
 
+
+def generate_csv_data_as_result(data: dict):
+    flattened_data = flatten_data(data)
     return get_csv(flattened_data)
 
 
 def generate_csv_data_as_results(data_list: list):
     flattened_data = []
     for data in data_list:
-        for question in data["questions"]:
-            row = {
-                "id": data["id"],
-                "id_user": data["id_user"],
-                "id_company": data["id_company"],
-                "id_quiz": data["id_quiz"],
-                "created_at": data["created_at"],
-                "question": question["question"],
-                "answer_is_correct": question["answer_is_correct"],
-                "user_answers": ", ".join(question["user_answers"])
-            }
-            flattened_data.append(row)
-
+        flattened_data.extend(flatten_data(data))
     return get_csv(flattened_data)
